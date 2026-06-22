58e35428cfdb1d3cd9b351da27c0144565fcc3c8
niconogzz <121233616+nnogue00@users.noreply.github.com>
2026-06-22 20:32:56 -0300

    Add Counter contract with increment function

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -1,1 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract Counter {
+
+    uint256 public count = 0;
+
+    function increment() public {
+        count = count + 1;
+    }
+}
