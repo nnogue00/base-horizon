e1d31b6e10b556fb16aa5999acc055fc2e9334e6
niconogzz <121233616+nnogue00@users.noreply.github.com>
2026-06-22 20:33:48 -0300

    Implement CounterV3 contract with increment, decrement, and reset

--- contracts/CounterV3.sol
+++ contracts/CounterV3.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract CounterV3 {
+
+    uint256 public count = 0;
+
+    function increment() public {
+        count = count + 1;
+    }
+
+    function decrement() public {
+        require(count > 0, "Count cannot be negative");
+
+        count = count - 1;
+    }
+
+    function reset() public {
+        count = 0;
+    }
+}
