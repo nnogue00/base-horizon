26f73e3b7ba4c9aad46bcc7c841d0ccb6c4cd4ef
niconogzz <121233616+nnogue00@users.noreply.github.com>
2026-06-22 20:57:06 -0300

    feat: add MessageBoard contract
    Added comments for Week 2 assignment regarding message storage and update.

--- contracts/MessageBoard.sol
+++ contracts/MessageBoard.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+/*
+Base Horizon - Week 2
+
+Message storage and update
+*/
+
+
 contract MessageBoard {
 
     string public message = "Welcome to Base Horizon!";
