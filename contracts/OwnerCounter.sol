c1bb5f830e1a491084f86d8072a75fa5c05bd2ca
niconogzz <121233616+nnogue00@users.noreply.github.com>
2026-06-22 20:34:37 -0300

    Add OwnerCounter contract with increment and reset functions

--- contracts/OwnerCounter.sol
+++ contracts/OwnerCounter.sol
@@ -1,1 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract OwnerCounter {
+
+    address public owner;
+    uint256 public count = 0;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function increment() public {
+        count = count + 1;
+    }
+
+    function reset() public {
+        require(msg.sender == owner, "Only owner can reset");
+
+        count = 0;
+    }
+}
