2cd7ac821ea94939150f27c97ffd25e2587f933d
niconogzz <121233616+nnogue00@users.noreply.github.com>
2026-06-22 20:55:26 -0300

    feat: add HelloBase contract
    Added a comment block for project context.

--- contracts/HelloBase.sol
+++ contracts/HelloBase.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+/*
+Base Horizon - Week 2
+
+First Solidity Contract
+*/
+
 contract HelloBase {
 
     string public message = "Hello Base Horizon!";
