3311edcdc40a5a53c0b2e247c4e3b60b279fce6b
niconogzz <121233616+nnogue00@users.noreply.github.com>
2026-06-24 21:37:01 -0300

    Implement UserProfile contract with profile management

--- contracts/UserProfileV2.sol
+++ contracts/UserProfileV2.sol
@@ -1,1 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract UserProfile {
+
+      
+    struct User {
+        string name;
+        uint256 age;
+    }
+
+    mapping(address => User) public users;
+
+    function createProfile(
+        string memory _name,
+        uint256 _age
+    ) public {
+
+        require(
+          bytes(users[msg.sender].name).length == 0,
+          "Profile already exists"
+        );
+
+        users[msg.sender] = User(_name, _age);
+    }
+
+    function updateProfile(
+        string memory _name,
+        uint256 _age
+    ) public {
+
+        require(
+          bytes(users[msg.sender].name).length > 0,
+          "Profile does not exist"
+        );
+
+        users[msg.sender] = User(_name, _age);
+    }
+
+    function getMyProfile()
+        public
+        view
+        returns (string memory, uint256)
+    {
+        User memory user = users[msg.sender];
+
+        return (user.name, user.age);
+    }
+}
