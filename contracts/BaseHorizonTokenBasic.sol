f8d9c774d9ee8c678103c309474dfbe0d451a987
niconogzz <121233616+nnogue00@users.noreply.github.com>
2026-06-26 18:43:10 -0300

    Create BaseHorizonTokenBasic contract with initial supply

--- contracts/BaseHorizonTokenBasic.sol
+++ contracts/BaseHorizonTokenBasic.sol
@@ -1,1 +1,12 @@
+contract BaseHorizonTokenBasic {
 
+string public name = "Base Horizon Token";
+string public symbol = "BHZN";
+uint256 public totalSupply = 1000000;
+
+mapping(address => uint256) public balances;
+
+    constructor(){
+        balances[msg.sender] = totalSupply;
+    }
+}
